2b96f62307d87cf678d7e5cc1b4b236bb12bb430
yasudar <yasuda@secmbp.local>
2015-06-29 14:28:01 +0900

    [remove] /users routeを削除

--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,7 @@
 module.exports = function(app, io) {
   var watchAuth = require('./watch-auth')(io);
-  app.get('/auth', watchAuth.auth);
+  app.get('/auth', watchAuth.auth); // debug
+  app.post('/auth', watchAuth.auth);
 
   // catch 404 and forward to error handler
   app.use(function(req, res, next) {
