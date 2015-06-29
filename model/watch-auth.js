31148d67a7290593ccf58e45f1cca8fbcea519e9
yasudar <yasuda@secmbp.local>
2015-06-29 15:37:40 +0900

    [modify] privateメソッド名の先頭をアンダースコアで開始するように変更

--- model/watch-auth.js
+++ model/watch-auth.js
@@ -1,48 +1,52 @@
 module.exports = (function() {
   var WatchAuth = function(io) {
-    this.io = io;
+    this._io = io;
   }
 
-  WatchAuth.prototype.distances = {};
+  WatchAuth.prototype._distances = {};
 
   WatchAuth.prototype.startConnection = function() {
-    this.io.on('connection', function(socket) {
-      this.onConnected(socket);
-      socket.on('disconnect', this.onDisconnected.bind(this));
-      socket.on('response-distance', this.onReceiveDistance.bind(this));
+    this._io.on('connection', function(socket) {
+      this._onConnected(socket);
+      socket.on('disconnect', this._onDisconnected.bind(this));
+      socket.on('response-distance', this._onReceiveDistance.bind(this));
     }.bind(this));
   }
 
   WatchAuth.prototype.auth = function(params, callback, timeout) {
-    this.requestDistance(function(distance) {
-      callback(this.checkPermission(params, distance));
+    this._requestDistance(function(distance) {
+      callback(this._checkPermission(params, distance));
     }.bind(this), timeout);
   }
 
-  WatchAuth.prototype.checkPermission = function(params, distance) {
+  WatchAuth.prototype._checkPermission = function(params, distance) {
     var result = {result: true, debug: distance};
     return result;
   }
 
-  WatchAuth.prototype.onConnected = function(socket) {
+  WatchAuth.prototype._onConnected = function(socket) {
     console.log("client connected!!");
   }
 
-  WatchAuth.prototype.onDisconnected = function() {
+  WatchAuth.prototype._onDisconnected = function() {
     console.log("client disconnected!!")
   }
 
-  WatchAuth.prototype.onReceiveDistance = function(data) {
-    this.distances[data['token']] = data['distance']
+  WatchAuth.prototype._onReceiveDistance = function(data) {
+    this._distances[data['token']] = data['distance']
     console.log(data);
   }
 
-  WatchAuth.prototype.requestDistance = function(callback, timeout) {
-    var token = '1234abc'
-    this.io.emit('request-distance', {token: token});
+  WatchAuth.prototype._generateToken = function() {
+    return '1234abc'
+  }
+
+  WatchAuth.prototype._requestDistance = function(callback, timeout) {
+    var token = this._generateToken()
+    this._io.emit('request-distance', {token: token});
 
     setTimeout(function() {
-      callback(this.distances[token]);
+      callback(this._distances[token]);
     }.bind(this), timeout);
   }
 
