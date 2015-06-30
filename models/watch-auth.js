ac0b26694e27ec4be6e6ae901f8f93bdd42b5377
ryysd <ry.ysd01@gmail.com>
2015-06-30 21:56:22 +0900

    [add] 複数クライアントにリクエストを投げるRequestDispacherクラスを追加

--- models/watch-auth.js
+++ models/watch-auth.js
@@ -1,44 +1,30 @@
 var SocketIO = require('../lib/socket-io.js');
+var RequestDispatcher = require('../lib/request-dispatcher.js');
 var Puid = require('puid');
 
 module.exports = (function() {
-  var _distances = {};
   var _socketIO = null;
+  var _requestDispatcher = null;
 
   var WatchAuth = function(io) {
     _socketIO = new SocketIO(io);
+    _requestDispatcher = new RequestDispatcher(_socketIO);
   }
 
   WatchAuth.prototype.startConnection = function() {
-    _socketIO.startConnection(function(socket) {
-      socket.on('response-distance', _onReceiveDistance.bind(this));
-    });
+    _socketIO.startConnection();
   }
 
   WatchAuth.prototype.auth = function(params, callback, timeout) {
-    _requestDistance.call(this, function(distance) {
-      callback(_checkPermission(params, distance));
-    }.bind(this), timeout);
+    var sockets = _socketIO.sockets.toArray();
+    _requestDispatcher.dispatch(sockets, 'distance', function(response) {
+      callback(response);
+    }, 1000);
   }
 
   var _checkPermission = function(params, distance) {
     var result = {result: true, debug: distance};
     return result;
-  }
-
-  var _onReceiveDistance = function(data) {
-    _distances[data.token] = data.distance
-    console.log(data);
-  }
-
-  var _requestDistance = function(callback, timeout) {
-    var token = (new Puid()).generate();
-    _socketIO.emit('request-distance', {token: token});
-
-    setTimeout(function() {
-      callback(_distances[token]);
-      delete _distances[token]
-    }.bind(this), timeout);
   }
 
   return WatchAuth;
