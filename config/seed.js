c977768838b7c36a10f692c7a7c7e2aa295007e1
ryysd <ry.ysd01@gmail.com>
2015-06-30 18:47:22 +0900

    [add] user seedにダミーのデバイスIDを追加

--- config/seed.js
+++ config/seed.js
@@ -18,10 +18,12 @@ db.roles.insert([
 db.users.insert([
   {
     name: 'admin',
+    deviceId: '1ABF357A-08C9-4D65-8EC5-18D2AB95E640',
     role: db.roles.findOne({name: 'admin'})._id
   },
   {
     name: 'user',
+    deviceId: 'AAAAAAAA-AAAa-AAAA-AAAA-AAAAAAAA',
     role: db.roles.findOne({name: 'user'})._id
   }
 ]);
