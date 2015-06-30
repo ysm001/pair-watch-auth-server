c9009ec991f75062a5190b08bd4f9c791a541834
ryysd <ry.ysd01@gmail.com>
2015-06-30 17:47:24 +0900

    [fix] schema.jsの構文ミスを修正

--- config/schema.js
+++ config/schema.js
@@ -1,9 +1,9 @@
 module.exports = function(db) {
   var Schema = db.Schema;
 
-  var Permission = new Schema(
+  var PermissionSchema = new Schema({
     name: {type: String, unique: true},
-  );
+  });
   db.model('Permission', PermissionSchema);
 
   var RoleSchema = new Schema({
