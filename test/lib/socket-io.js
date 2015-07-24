a2bba6fffb00e90200c9925c194a181a3dc0c8f6
ryysd <ry.ysd01@gmail.com>
2015-07-24 18:42:27 +0900

    [add] SocketIOにテストを追加
    接続時にsocket.hansshake.user.idにIDが格納されていることをテスト

--- test/lib/socket-io.js
+++ test/lib/socket-io.js
@@ -20,44 +20,56 @@ var spawnClients = function(users) {
   return users.map(function(user) {ClientHelper.spawn(user)});
 };
 
+var doTestWithUser = function(users, testFunction) {
+  var clients = spawnClients(users);
+
+  setTimeout(function() {
+    testFunction(clients);
+  }, 1000);
+};
+
 describe('SocketIO', function () {
   describe('sockets', function () {
-    it('繧ｯ繝ｩ繧､繧｢繝ｳ繝域悴謗･邯壽凾縺ｮsockets縺ｮ繧ｵ繧､繧ｺ縺ｯ0', function () {
+    it('クライアント未接続時のsocketsのサイズは0', function () {
       assert.equal(socketIO.sockets().length, 0);
     });
 
-    it('2繧ｯ繝ｩ繧､繧｢繝ｳ繝域磁邯壽凾縺ｮsockets縺ｮ繧ｵ繧､繧ｺ縺ｯ2', function (done) {
+    it('2クライアント接続時のsocketsのサイズは2', function (done) {
       var users = ['READONLY-USER-A', 'READONLY-USER-B'];
-      var clients = spawnClients(users);
 
-      setTimeout(function() {
+      doTestWithUser(users, function(clients) {
         assert.equal(socketIO.sockets().length, 2);
         done();
-      }, 1000);
+      });
     });
   });
 
   describe('socket', function () {
     it('接続済のsocketが全て取得可能かつ接続していないsocketの情報は取得不可能', function (done) {
-      var users = [
-        'READONLY-USER-A',
-        'READONLY-USER-B',
-        'USER-A',
-        'USER-B',
-        'ADMIN-USER-A'
-      ];
+      var users = 
+        ['READONLY-USER-A', 'READONLY-USER-B',
+          'USER-A', 'USER-B', 'ADMIN-USER-A'];
 
-      var clients = spawnClients(users);
-
-      setTimeout(function() {
+      doTestWithUser(users, function(clients) {
         users.forEach(function(user) {
           assert.notEqual(socketIO.socket(user), null);
         });
 
         assert.equal(socketIO.socket('ADMIN-USER-B'), null);
+        done();
+      });
+    });
+
+    it('接続済クライアントのsocketにはIDが紐付けられている', function (done) {
+      var users = ['READONLY-USER-A', 'READONLY-USER-B'];
+
+      doTestWithUser(users, function(clients) {
+        users.forEach(function(user) {
+          assert.equal(socketIO.socket(user).handshake.user.id, user);
+        });
 
         done();
-      }, 1000);
+      });
     });
   });
 });
