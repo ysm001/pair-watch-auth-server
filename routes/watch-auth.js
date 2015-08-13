a090094260a6ed5280713959543e9b80c3cd89dc
ryysd <ry.ysd01@gmail.com>
2015-08-13 16:17:36 +0900

    [modify] Errorを投げていた箇所を、Custom Errorに変更

--- routes/watch-auth.js
+++ routes/watch-auth.js
@@ -9,12 +9,14 @@ module.exports = function(io) {
   return {
     auth: function(req, res) {
       watchAuth.auth(req.body.id, req.body.permission, 2000).then(function(result) {
-        AuthResultNotificator.notify(watchAuth.socketIO(), req.body.id, req.body.permission, result, result.requiredUsers, result.nearPermissionHolders)
+        AuthResultNotificator.notify(watchAuth.socketIO(), req.body.id, req.body.permission, result.accessibility, result.requiredUsers, result.nearPermissionHolders)
 
         res.send({
           result: result.accessibility,
         })
       }).catch(function(err) {
+        AuthResultNotificator.notify(watchAuth.socketIO(), req.body.id, req.body.permission, false, err.requiredUsers, err.nearPermissionHolders)
+
         res.send({
           result: false,
           error: err 
